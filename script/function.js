2ad8b3d8889f81070195862f1c751251b7e4a690
goziyev <goziyevmuhammadgozi@gmail.com>
2024-01-20 11:18:58 +0500

    task completed

--- script/function.js
+++ script/function.js
@@ -1,5 +1,5 @@
-function createCard(element){
-    return`
+function createCard(element) {
+  return `
     <div class="card" id=${element.name.slug}>
     <img class="card-img" src=${element.flags.png} width="264" height="160" alt="" />
     <div class="card-texts">
@@ -12,44 +12,62 @@ function createCard(element){
     <p class="card-description">Capital: <span>${element.capital}</span></p>
   
   </div>
-  </div>`   
-  }
+  </div>`;
+}
 
-function createCardDetail(element){  
-    return `
+function createCardDetail(element) {
+  return `
     <div class="in-detail container" id="in-detail-wrapper">
     <div class="indetail_img">
-        <img src=${element.flags.png} width="559" height="483" alt=${element.flags.alt} >
+        <img src=${element.flags.png} width="559" height="483" alt=${
+    element.flags.alt
+  } >
     </div>
     <div class="in-detail__texts">
         <h1>${element.name.common}</h1>
         <div class="in-detail-flex">
             <div>
-                <p class="card-description fonts">Native Name: <span>${element.name.nativeName}</span>
+                <p class="card-description fonts">Native Name: <span>${
+                  element.name.nativeName
+                }</span>
                 </p>
-                <p class="card-description fonts">Population: <span>${element.population}</span></p>
-                <p class="card-description fonts">Region: <span>${element.region}</span></p>
-                <p class="card-description fonts">Sub Region: <span>${element.subregion }</span></p>
-                <p class="card-description fonts">Capital: <span>${element.capital}</span></p>
+                <p class="card-description fonts">Population: <span>${
+                  element.population
+                }</span></p>
+                <p class="card-description fonts">Region: <span>${
+                  element.region
+                }</span></p>
+                <p class="card-description fonts">Sub Region: <span>${
+                  element.subregion
+                }</span></p>
+                <p class="card-description fonts">Capital: <span>${
+                  element.capital
+                }</span></p>
               
             </div>
             <div>
                 <p class="card-description fonts">Top Level Domain: <span>.be</span>
                 </p>
-                <p class="card-description fonts">Currencies: <span>${element.currencies}</span></p>
-                <p class="card-description fonts">Languages: <span>${element.languages}</span></p>
+                <p class="card-description fonts">Currencies: <span>${
+                  element.currencies
+                }</span></p>
+                <p class="card-description fonts">Languages: <span>${
+                  element.languages
+                }</span></p>
 
             </div>
                 
         </div>
         <div class="border-countries">
             <p class="card-description">Border Countries:</p>
-            ${element.borders.map(el => {return ` <p class="card-border-countries" id=${el.slug}>${el.common}</p>`})}
+            ${element.borders.map((el) => {
+              return ` <p class="card-border-countries" id=${el.slug}>${el.common}</p>`;
+            })}
         </div>
     </div>
 </div>
-`
+`;
 }
 
 
-  export{createCard,createCardDetail}
+export { createCard, createCardDetail };
--- script/script.js
+++ script/script.js
@@ -1,7 +1,16 @@
 import { createCard } from "./function.js";
-
+const mainInput  = document.getElementById('main-input');
+const toggle = document.querySelector('.toggle')
 const wrapper = document.getElementById("cards-wrapper");
-const maiInput  = document.getElementById('main-input');
+const filterRegion = document.getElementById('filter_select');
+
+toggle && toggle.addEventListener('click',e =>{
+  toggle.setAttribute('class','dark-mode')
+  if (toggle.getAttribute('class') == "dark-mode") {
+    document.body.style.background = "green"
+  }
+})
+
 
 document.addEventListener("DOMContentLoaded", function () {
   fetch("https://countries-api-v7sn.onrender.com/countries?limit=250",{
@@ -28,4 +37,61 @@ document.addEventListener("DOMContentLoaded", function () {
       console.log(err);
     });
 });
+
+
+function myScript() {
+  fetch(
+    `https://countries-api-v7sn.onrender.com/countries?search=${mainInput.value}`
+  )
+    .then((data) => data.json())
+    .then((data) => {
+      wrapper.innerHTML = "";
+      data.data.forEach((element) => {
+        let card = createCard(element);
+        wrapper.innerHTML += card;
+      });
+      const cards = document.querySelectorAll(".card");
+      cards.forEach((el) => {
+        el.addEventListener("click", function () {
+          let elId = this.getAttribute("id");
+          window.location.assign(`./pages/detail.html?id${elId}`);
+        });
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+
+function myfilterScript() {
+    fetch(
+      `https://countries-api-v7sn.onrender.com/countries?region=${filterRegion.value}`
+    )
+      .then((data) => data.json())
+      .then((data) => {
+        wrapper.innerHTML = "";
+        data.data.forEach((element) => {
+          let card = createCard(element);
+          wrapper.innerHTML += card;
+        });
+        const cards = document.querySelectorAll(".card");
+        cards.forEach((el) => {
+          el.addEventListener("click", function () {
+            let elId = this.getAttribute("id");
+            window.location.assign(`./pages/detail.html?id${elId}`);
+          });
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+mainInput.addEventListener("keyup", myScript);
+
+filterRegion.addEventListener('change',myfilterScript)
+
+
+
 
