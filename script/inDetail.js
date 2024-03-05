aad905b66ca3fcfd1e297d9f6f681040a2fb42f0
goziyev <goziyevmuhammadgozi@gmail.com>
2024-03-05 14:54:26 +0500

    task completed

--- script/inDetail.js
+++ script/inDetail.js
@@ -2,66 +2,72 @@ import { createCardDetail } from "./function.js";
 let main = document.getElementById("main");
 let back = document.getElementById("back");
 
-const body = document.querySelector('body');
-const modeImg = document.querySelector('.mode-img')
-const mainLoader  = document.querySelector(".main-loader")
-const loader  = document.querySelector(".loader")
+const body = document.querySelector("body");
+const modeImg = document.querySelector(".mode-img");
+const mainLoader = document.querySelector(".main-loader");
+const loader = document.querySelector(".loader");
 
-
-mainLoader.style.display= "none";
+mainLoader.style.display = "none";
 
 function loaderCallback() {
-loader.style.display= "none";
-mainLoader.style.display= "block";
+  loader.style.display = "none";
+  mainLoader.style.display = "block";
 }
 
-
-
-
-let icon  = document.querySelector('.mode');
-icon.onclick = function(){
-  body.classList.toggle("dark-theme")
-  if (body.classList.contains('dark-theme')) {
-    modeImg.setAttribute("src","../icons/sun.png")
-  }else{
-    modeImg.setAttribute("src","../icons/sun.png")
-    
+let icon = document.querySelector(".mode");
+icon.onclick = function () {
+  body.classList.toggle("dark-theme");
+  if (body.classList.contains("dark-theme")) {
+    modeImg.setAttribute("src", "../icons/sun.png");
+  } else {
+    modeImg.setAttribute("src", "../icons/sun.png");
   }
-}
-
-
+};
 
 back &&
   back.addEventListener("click", function () {
     window.location.assign("../index.html");
   });
 
-document
-  .addEventListener("DOMContentLoaded", function () {
-    let location = window.location.href.substring(
-      window.location.href.search("id") + 2
-    );
-    if (!location.length) {
-        
+document.addEventListener("DOMContentLoaded", function () {
+  if (localStorage.getItem("mode") == "dark-theme") {
+    body.classList = "dark-theme";
+    if (body.classList.contains("dark-theme")) {
+      modeImg.setAttribute("src", "../icons/sun.png");
+    } else {
+      modeImg.setAttribute("src", "../icons/moon.png");
     }
-    fetch(`https://countries-api-v7sn.onrender.com/countries/slug/${location}`)
-      .then((res) => res.json())
+  }
 
-      .then((data) => {
-        main.innerHTML = createCardDetail(data);
-        let  allButtons  =  document.querySelectorAll("p.card-border-countries");
-        if (allButtons.length) {
-         allButtons.forEach((el) => {
-             el.addEventListener("click", function () {
-                 let btn = this.getAttribute("id");
-                 console.log(btn);
-                     window.location.assign(`./detail.html?id${btn}`);
-               });
-           });
-       }
-       loaderCallback();
-      })
-    //   .catch(window.location.assign("../index.html"));
-    
-     
-})
+  if (localStorage.getItem("mode") == "white-theme") {
+    body.classList.toggle("white-theme");
+    if (body.classList.contains("white-theme")) {
+      modeImg.setAttribute("src", "../icons/moon.png");
+    } else {
+      modeImg.setAttribute("src", "../icons/sunn.png");
+    }
+  }
+  let location = window.location.href.substring(
+    window.location.href.search("id") + 2
+  );
+  if (!location.length) {
+  }
+  fetch(`https://frontend-mentor-apis-6efy.onrender.com/countries/${location}`)
+    .then((res) => res.json())
+
+    .then((data) => {
+      main.innerHTML = createCardDetail(data);
+      let allButtons = document.querySelectorAll("p.card-border-countries");
+      if (allButtons.length) {
+        allButtons.forEach((el) => {
+          el.addEventListener("click", function () {
+            let btn = this.getAttribute("id");
+            console.log(btn);
+            window.location.assign(`./detail.html?id${btn}`);
+          });
+        });
+      }
+      loaderCallback();
+    });
+  //   .catch(window.location.assign("../index.html"));
+});
--- script/script.js
+++ script/script.js
@@ -1,93 +1,86 @@
 import { createCard } from "./function.js";
-const mainInput  = document.getElementById('main-input');
+const mainInput = document.getElementById("main-input");
 const wrapper = document.getElementById("cards-wrapper");
-const filterRegion = document.getElementById('filter_select');
-const search = document.querySelector('.search')
-const header = document.getElementById('header')
-const body = document.querySelector('body');
-const modeImg = document.querySelector('.mode-img')
-const loader  = document.querySelector(".loader")
-const mainLoader  = document.querySelector(".main-loader")
-mainLoader.style.display= "none";
+const filterRegion = document.getElementById("filter_select");
+const search = document.querySelector(".search");
+const header = document.getElementById("header");
+const body = document.querySelector("body");
+const modeImg = document.querySelector(".mode-img");
+const loader = document.querySelector(".loader");
+const mainLoader = document.querySelector(".main-loader");
+mainLoader.style.display = "none";
 
 function loaderCallback() {
-loader.style.display= "none";
-mainLoader.style.display= "block";
+  loader.style.display = "none";
+  mainLoader.style.display = "block";
 }
 
+let icon = document.querySelector(".mode");
+icon.onclick = function () {
+  let a = localStorage.getItem("mode");
+  body.classList.toggle("dark-theme");
 
+  localStorage.setItem(
+    "mode",
+    `${
+      localStorage.getItem("mode") == "dark-theme"
+        ? "white-theme"
+        : "dark-theme"
+    }`
+  );
 
-let icon  = document.querySelector('.mode');
-icon.onclick = function(){
-  
-  
-  let a = localStorage.getItem('mode')
-  body.classList.toggle('dark-theme');
-  
-  
-  localStorage.setItem('mode',`${localStorage.getItem('mode') == 'dark-theme' ? 'white-theme' : "dark-theme" }`)
-  
-  if (body.classList.contains('dark-theme')) {
-    modeImg.setAttribute("src","../icons/sun.png")
-  }else{
-    modeImg.setAttribute("src","../icons/moon.png")
+  if (body.classList.contains("dark-theme")) {
+    modeImg.setAttribute("src", "../icons/sun.png");
+  } else {
+    modeImg.setAttribute("src", "../icons/moon.png");
   }
-}
-
-
+};
 
 document.addEventListener("DOMContentLoaded", function () {
-  if (localStorage.getItem('mode') == 'dark-theme') {
+  if (localStorage.getItem("mode") == "dark-theme") {
     body.classList = "dark-theme";
-    if (body.classList.contains('dark-theme')) {
-      modeImg.setAttribute("src","../icons/sun.png")
-    }else{
-      modeImg.setAttribute("src","../icons/moon.png")
+    if (body.classList.contains("dark-theme")) {
+      modeImg.setAttribute("src", "../icons/sun.png");
+    } else {
+      modeImg.setAttribute("src", "../icons/moon.png");
     }
-}
-   
-if (localStorage.getItem('mode') == 'white-theme') {
-  body.classList.toggle("white-theme");
-  if (body.classList.contains('white-theme')) {
-    modeImg.setAttribute("src","../icons/moon.png")
-  }else{
-    modeImg.setAttribute("src","../icons/sunn.png")
   }
-}
-  
-  fetch("https://countries-api-v7sn.onrender.com/countries?limit=250",{
-    method:"GET"
+
+  if (localStorage.getItem("mode") == "white-theme") {
+    body.classList.toggle("white-theme");
+    if (body.classList.contains("white-theme")) {
+      modeImg.setAttribute("src", "../icons/moon.png");
+    } else {
+      modeImg.setAttribute("src", "../icons/sunn.png");
+    }
+  }
+
+  fetch("https://frontend-mentor-apis-6efy.onrender.com/countries", {
+    method: "GET",
   })
-  .then((res) => res.json())
-  .then((data) => {
-  
-      data.data.forEach(element => {
-        
+    .then((res) => res.json())
+    .then((data) => {
+      data.data.forEach((element) => {
         let card = createCard(element);
-        wrapper.innerHTML += card
+        wrapper.innerHTML += card;
       });
       const cards = document.querySelectorAll(".card");
-      cards.forEach(el => {
+      cards.forEach((el) => {
         el.addEventListener("click", function () {
           let elId = this.getAttribute("id");
           window.location.assign(`./pages/detail.html?id${elId}`);
         });
         loaderCallback();
       });
-      
-    
-     
-      
     })
     .catch((err) => {
       console.log(err);
     });
 });
 
-
 function myScript() {
   fetch(
-    `https://countries-api-v7sn.onrender.com/countries?search=${mainInput.value}`
+    `https://frontend-mentor-apis-6efy.onrender.com/countries?search=${mainInput.value}`
   )
     .then((data) => data.json())
     .then((data) => {
@@ -109,35 +102,30 @@ function myScript() {
     });
 }
 
-
 function myfilterScript() {
-    fetch(
-      `https://countries-api-v7sn.onrender.com/countries?region=${filterRegion.value}`
-    )
-      .then((data) => data.json())
-      .then((data) => {
-        wrapper.innerHTML = "";
-        data.data.forEach((element) => {
-          let card = createCard(element);
-          wrapper.innerHTML += card;
-        });
-        const cards = document.querySelectorAll(".card");
-        cards.forEach((el) => {
-          el.addEventListener("click", function () {
-            let elId = this.getAttribute("id");
-            window.location.assign(`./pages/detail.html?id${elId}`);
-          });
-        });
-      })
-      .catch((err) => {
-        console.log(err);
+  fetch(
+    `https://frontend-mentor-apis-6efy.onrender.com/countries?region=${filterRegion.value}`
+  )
+    .then((data) => data.json())
+    .then((data) => {
+      wrapper.innerHTML = "";
+      data.data.forEach((element) => {
+        let card = createCard(element);
+        wrapper.innerHTML += card;
       });
-  }
+      const cards = document.querySelectorAll(".card");
+      cards.forEach((el) => {
+        el.addEventListener("click", function () {
+          let elId = this.getAttribute("id");
+          window.location.assign(`./pages/detail.html?id${elId}`);
+        });
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
 
 mainInput.addEventListener("keyup", myScript);
 
-filterRegion.addEventListener('change',myfilterScript)
-
-
-
-
+filterRegion.addEventListener("change", myfilterScript);
